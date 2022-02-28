9b9673b8e402847d57462d11160d13c03f41dc32
aboudiwan <steven.aboudiwan.ext@sogeclairaerospace.com>
2022-02-28 14:29:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+"# Project-6" 
+"# Project-6" 
+"# Project-6" 
+"# Project-6" 
---
+++ init_git.bat
@@ -0,0 +1,7 @@
+echo "# Project-6" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/aboustev/OPC-Project_7.git
+git push -u origin main
