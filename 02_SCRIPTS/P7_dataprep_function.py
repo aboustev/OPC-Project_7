31f9907dcd713adae5b2f1b7e908a3cbbef6b223
aboudiwan <steven.aboudiwan.ext@sogeclairaerospace.com>
2022-03-21 17:03:05 +0100

    update 7 : First implementation of fbeta score Creation of 2 python scripts to host the functions train_test_grid becomes TrainTestGrid

---
+++ 02_SCRIPTS/P7_dataprep_function.py
@@ -0,0 +1,465 @@
+"""
+The purpose of this python file is to regroup every functions from the external kernel
+"""
+import numpy as np
+import pandas as pd
+import gc
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+import json
+import os
+import cv2
+import gc
+from contextlib import contextmanager
+from lightgbm import LGBMClassifier
+from lightgbm import log_evaluation
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold, StratifiedKFold
+path_to_data = r'..\01_DATA'
+
+
+@contextmanager
+def timer(title):
+    """
+    timer function
+    :param title:
+    """
+    t0 = time.time()
+    yield
+    print("{} - done in {:.0f}s".format(title, time.time() - t0))
+
+
+def one_hot_encoder(df, nan_as_category=True):
+    """
+    One hot encode columns from df
+    :param df:
+    :param nan_as_category:
+    :return:
+    """
+    original_columns = list(df.columns)
+    categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
+    df = pd.get_dummies(df, columns=categorical_columns, dummy_na=nan_as_category)
+    new_columns = [c for c in df.columns if c not in original_columns]
+    return df, new_columns
+
+
+def application_train_test(num_rows=None, nan_as_category=False):
+    """
+    Import csv files application train and application test and prepare data
+    :param num_rows:
+    :param nan_as_category:
+    :return:
+    """
+    # Read data and merge
+    df = pd.read_csv(os.path.join(path_to_data, 'application_train.csv'), nrows=num_rows)
+    test_df = pd.read_csv(os.path.join(path_to_data, 'application_test.csv'), nrows=num_rows)
+    print("Train samples: {}, test samples: {}".format(len(df), len(test_df)))
+    df = df.append(test_df).reset_index()
+    # Optional: Remove 4 applications with XNA CODE_GENDER (train set)
+    df = df[df['CODE_GENDER'] != 'XNA']
+
+    # Categorical features with Binary encode (0 or 1; two categories)
+    for bin_feature in ['CODE_GENDER', 'FLAG_OWN_CAR', 'FLAG_OWN_REALTY']:
+        df[bin_feature], uniques = pd.factorize(df[bin_feature])
+    # Categorical features with One-Hot encode
+    df, cat_cols = one_hot_encoder(df, nan_as_category)
+
+    # NaN values for DAYS_EMPLOYED: 365.243 -> nan
+    df['DAYS_EMPLOYED'].replace(365243, np.nan, inplace=True)
+    # Some simple new features (percentages)
+    df['DAYS_EMPLOYED_PERC'] = df['DAYS_EMPLOYED'] / df['DAYS_BIRTH']
+    df['INCOME_CREDIT_PERC'] = df['AMT_INCOME_TOTAL'] / df['AMT_CREDIT']
+    df['INCOME_PER_PERSON'] = df['AMT_INCOME_TOTAL'] / df['CNT_FAM_MEMBERS']
+    df['ANNUITY_INCOME_PERC'] = df['AMT_ANNUITY'] / df['AMT_INCOME_TOTAL']
+    df['PAYMENT_RATE'] = df['AMT_ANNUITY'] / df['AMT_CREDIT']
+    del test_df
+    gc.collect()
+    return df
+
+
+def bureau_and_balance(num_rows=None):
+    """
+    Import csv files bureau and bureau balance and prepare data
+    :param num_rows:
+    :return:
+    """
+    bureau = pd.read_csv(
+        os.path.join(path_to_data, 'bureau.csv'), nrows=num_rows)
+    bb = pd.read_csv(
+        os.path.join(path_to_data, 'bureau_balance.csv'), nrows=num_rows)
+    bb, bb_cat = one_hot_encoder(bb)
+    bureau, bureau_cat = one_hot_encoder(bureau)
+
+    # Bureau balance: Perform aggregations and merge with bureau.csv
+    bb_aggregations = {'MONTHS_BALANCE': ['min', 'max', 'size']}
+    for col in bb_cat:
+        bb_aggregations[col] = ['mean']
+    bb_agg = bb.groupby('SK_ID_BUREAU').agg(bb_aggregations)
+    bb_agg.columns = pd.Index([e[0] + "_" + e[1].upper() for e in bb_agg.columns.tolist()])
+    bureau = bureau.join(bb_agg, on='SK_ID_BUREAU')
+    bureau.drop(['SK_ID_BUREAU'], axis=1, inplace=True)
+    del bb, bb_agg
+    gc.collect()
+
+    # Bureau and bureau_balance numeric features
+    num_aggregations = {
+        'DAYS_CREDIT': ['min', 'max', 'mean', 'var'],
+        'DAYS_CREDIT_ENDDATE': ['min', 'max', 'mean'],
+        'DAYS_CREDIT_UPDATE': ['mean'],
+        'CREDIT_DAY_OVERDUE': ['max', 'mean'],
+        'AMT_CREDIT_MAX_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM': ['max', 'mean', 'sum'],
+        'AMT_CREDIT_SUM_DEBT': ['max', 'mean', 'sum'],
+        'AMT_CREDIT_SUM_OVERDUE': ['mean'],
+        'AMT_CREDIT_SUM_LIMIT': ['mean', 'sum'],
+        'AMT_ANNUITY': ['max', 'mean'],
+        'CNT_CREDIT_PROLONG': ['sum'],
+        'MONTHS_BALANCE_MIN': ['min'],
+        'MONTHS_BALANCE_MAX': ['max'],
+        'MONTHS_BALANCE_SIZE': ['mean', 'sum']
+    }
+    # Bureau and bureau_balance categorical features
+    cat_aggregations = {}
+    for cat in bureau_cat:
+        cat_aggregations[cat] = ['mean']
+    for cat in bb_cat:
+        cat_aggregations[cat + "_MEAN"] = ['mean']
+
+    bureau_agg = bureau.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    bureau_agg.columns = pd.Index(['BURO_' + e[0] + "_" + e[1].upper() for e in bureau_agg.columns.tolist()])
+    # Bureau: Active credits - using only numerical aggregations
+    active = bureau[bureau['CREDIT_ACTIVE_Active'] == 1]
+    active_agg = active.groupby('SK_ID_CURR').agg(num_aggregations)
+    active_agg.columns = pd.Index(['ACTIVE_' + e[0] + "_" + e[1].upper() for e in active_agg.columns.tolist()])
+    bureau_agg = bureau_agg.join(active_agg, how='left', on='SK_ID_CURR')
+    del active, active_agg
+    gc.collect()
+    # Bureau: Closed credits - using only numerical aggregations
+    closed = bureau[bureau['CREDIT_ACTIVE_Closed'] == 1]
+    closed_agg = closed.groupby('SK_ID_CURR').agg(num_aggregations)
+    closed_agg.columns = pd.Index(['CLOSED_' + e[0] + "_" + e[1].upper() for e in closed_agg.columns.tolist()])
+    bureau_agg = bureau_agg.join(closed_agg, how='left', on='SK_ID_CURR')
+    del closed, closed_agg, bureau
+    gc.collect()
+    return bureau_agg
+
+
+def previous_applications(num_rows=None):
+    """
+    Import csv file previous applications and prepare data
+    :param num_rows:
+    :return:
+    """
+    prev = pd.read_csv(
+        os.path.join(path_to_data, 'previous_application.csv'), nrows=num_rows)
+    prev, cat_cols = one_hot_encoder(prev)
+    # Days 365.243 values -> nan
+    prev['DAYS_FIRST_DRAWING'].replace(365243, np.nan, inplace=True)
+    prev['DAYS_FIRST_DUE'].replace(365243, np.nan, inplace=True)
+    prev['DAYS_LAST_DUE_1ST_VERSION'].replace(365243, np.nan, inplace=True)
+    prev['DAYS_LAST_DUE'].replace(365243, np.nan, inplace=True)
+    prev['DAYS_TERMINATION'].replace(365243, np.nan, inplace=True)
+    # Add feature: value ask / value received percentage
+    prev['APP_CREDIT_PERC'] = prev['AMT_APPLICATION'] / prev['AMT_CREDIT']
+    # Previous applications numeric features
+    num_aggregations = {
+        'AMT_ANNUITY': ['min', 'max', 'mean'],
+        'AMT_APPLICATION': ['min', 'max', 'mean'],
+        'AMT_CREDIT': ['min', 'max', 'mean'],
+        'APP_CREDIT_PERC': ['min', 'max', 'mean', 'var'],
+        'AMT_DOWN_PAYMENT': ['min', 'max', 'mean'],
+        'AMT_GOODS_PRICE': ['min', 'max', 'mean'],
+        'HOUR_APPR_PROCESS_START': ['min', 'max', 'mean'],
+        'RATE_DOWN_PAYMENT': ['min', 'max', 'mean'],
+        'DAYS_DECISION': ['min', 'max', 'mean'],
+        'CNT_PAYMENT': ['mean', 'sum'],
+    }
+    # Previous applications categorical features
+    cat_aggregations = {}
+    for cat in cat_cols:
+        cat_aggregations[cat] = ['mean']
+
+    prev_agg = prev.groupby('SK_ID_CURR').agg({**num_aggregations, **cat_aggregations})
+    prev_agg.columns = pd.Index(['PREV_' + e[0] + "_" + e[1].upper() for e in prev_agg.columns.tolist()])
+    # Previous Applications: Approved Applications - only numerical features
+    approved = prev[prev['NAME_CONTRACT_STATUS_Approved'] == 1]
+    approved_agg = approved.groupby('SK_ID_CURR').agg(num_aggregations)
+    approved_agg.columns = pd.Index(['APPROVED_' + e[0] + "_" + e[1].upper() for e in approved_agg.columns.tolist()])
+    prev_agg = prev_agg.join(approved_agg, how='left', on='SK_ID_CURR')
+    # Previous Applications: Refused Applications - only numerical features
+    refused = prev[prev['NAME_CONTRACT_STATUS_Refused'] == 1]
+    refused_agg = refused.groupby('SK_ID_CURR').agg(num_aggregations)
+    refused_agg.columns = pd.Index(['REFUSED_' + e[0] + "_" + e[1].upper() for e in refused_agg.columns.tolist()])
+    prev_agg = prev_agg.join(refused_agg, how='left', on='SK_ID_CURR')
+    del refused, refused_agg, approved, approved_agg, prev
+    gc.collect()
+    return prev_agg
+
+
+def pos_cash(num_rows=None):
+    """
+    Import csv file pos cash balance and prepare data
+    :param num_rows:
+    :return:
+    """
+    pos = pd.read_csv(
+        os.path.join(path_to_data, 'POS_CASH_balance.csv'), nrows=num_rows)
+    pos, cat_cols = one_hot_encoder(pos)
+    # Features
+    aggregations = {
+        'MONTHS_BALANCE': ['max', 'mean', 'size'],
+        'SK_DPD': ['max', 'mean'],
+        'SK_DPD_DEF': ['max', 'mean']
+    }
+    for cat in cat_cols:
+        aggregations[cat] = ['mean']
+
+    pos_agg = pos.groupby('SK_ID_CURR').agg(aggregations)
+    pos_agg.columns = pd.Index(['POS_' + e[0] + "_" + e[1].upper() for e in pos_agg.columns.tolist()])
+    # Count pos cash accounts
+    pos_agg['POS_COUNT'] = pos.groupby('SK_ID_CURR').size()
+    del pos
+    gc.collect()
+    return pos_agg
+
+
+def installments_payments(num_rows=None):
+    """
+    Import csv file installments payments and prepare data
+    :param num_rows:
+    :return:
+    """
+    ins = pd.read_csv(
+        os.path.join(path_to_data, 'installments_payments.csv'), nrows=num_rows)
+    ins, cat_cols = one_hot_encoder(ins)
+    # Percentage and difference paid in each installment (amount paid and installment value)
+    ins['PAYMENT_PERC'] = ins['AMT_PAYMENT'] / ins['AMT_INSTALMENT']
+    ins['PAYMENT_DIFF'] = ins['AMT_INSTALMENT'] - ins['AMT_PAYMENT']
+    # Days past due and days before due (no negative values)
+    ins['DPD'] = ins['DAYS_ENTRY_PAYMENT'] - ins['DAYS_INSTALMENT']
+    ins['DBD'] = ins['DAYS_INSTALMENT'] - ins['DAYS_ENTRY_PAYMENT']
+    ins['DPD'] = ins['DPD'].apply(lambda x: x if x > 0 else 0)
+    ins['DBD'] = ins['DBD'].apply(lambda x: x if x > 0 else 0)
+    # Features: Perform aggregations
+    aggregations = {
+        'NUM_INSTALMENT_VERSION': ['nunique'],
+        'DPD': ['max', 'mean', 'sum'],
+        'DBD': ['max', 'mean', 'sum'],
+        'PAYMENT_PERC': ['max', 'mean', 'sum', 'var'],
+        'PAYMENT_DIFF': ['max', 'mean', 'sum', 'var'],
+        'AMT_INSTALMENT': ['max', 'mean', 'sum'],
+        'AMT_PAYMENT': ['min', 'max', 'mean', 'sum'],
+        'DAYS_ENTRY_PAYMENT': ['max', 'mean', 'sum']
+    }
+    for cat in cat_cols:
+        aggregations[cat] = ['mean']
+    ins_agg = ins.groupby('SK_ID_CURR').agg(aggregations)
+    ins_agg.columns = pd.Index(['INSTAL_' + e[0] + "_" + e[1].upper() for e in ins_agg.columns.tolist()])
+    # Count installments accounts
+    ins_agg['INSTAL_COUNT'] = ins.groupby('SK_ID_CURR').size()
+    del ins
+    gc.collect()
+    return ins_agg
+
+
+def credit_card_balance(num_rows=None):
+    """
+    Import csv file credit card balance and prepare data
+    :param num_rows:
+    :return:
+    """
+    cc = pd.read_csv(
+        os.path.join(path_to_data, 'credit_card_balance.csv'), nrows=num_rows)
+    cc, cat_cols = one_hot_encoder(cc)
+    # General aggregations
+    cc.drop(['SK_ID_PREV'], axis=1, inplace=True)
+    cc_agg = cc.groupby('SK_ID_CURR').agg(['min', 'max', 'mean', 'sum', 'var'])
+    cc_agg.columns = pd.Index(['CC_' + e[0] + "_" + e[1].upper() for e in cc_agg.columns.tolist()])
+    # Count credit card lines
+    cc_agg['CC_COUNT'] = cc.groupby('SK_ID_CURR').size()
+    del cc
+    gc.collect()
+    return cc_agg
+
+
+def kfold_lightgbm(df, num_folds, stratified=False, debug=False):
+    """
+    Function to determine the importance of features
+    :param df:
+    :param num_folds:
+    :param stratified:
+    :param debug:
+    :return:
+    """
+    submission_file_name = r'..\01_DATA\kfold_results.csv'
+    # Divide in training/validation and test data
+    train_df = df[df['TARGET'].notnull()]
+    test_df = df[df['TARGET'].isnull()]
+    print("Starting LightGBM. Train shape: {}, test shape: {}".format(train_df.shape, test_df.shape))
+    del df
+    gc.collect()
+    # Cross validation model
+    if stratified:
+        folds = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=1001)
+    else:
+        folds = KFold(n_splits=num_folds, shuffle=True, random_state=1001)
+    # Create arrays and dataframes to store results
+    oof_preds = np.zeros(train_df.shape[0])
+    sub_preds = np.zeros(test_df.shape[0])
+    feature_importance_df = pd.DataFrame()
+    feats = [f for f in train_df.columns if f not in ['TARGET', 'SK_ID_CURR', 'SK_ID_BUREAU', 'SK_ID_PREV', 'index']]
+
+    for n_fold, (train_idx, valid_idx) in enumerate(folds.split(train_df[feats], train_df['TARGET'])):
+        train_x, train_y = train_df[feats].iloc[train_idx], train_df['TARGET'].iloc[train_idx]
+        valid_x, valid_y = train_df[feats].iloc[valid_idx], train_df['TARGET'].iloc[valid_idx]
+
+        # LightGBM parameters found by Bayesian optimization
+        clf = LGBMClassifier(
+            n_estimators=10000,
+            learning_rate=0.02,
+            num_leaves=34,
+            colsample_bytree=0.9497036,
+            subsample=0.8715623,
+            max_depth=8,
+            reg_alpha=0.041545473,
+            reg_lambda=0.0735294,
+            min_split_gain=0.0222415,
+            min_child_weight=39.3259775)
+        callback = [log_evaluation(period=0)]
+        clf.fit(train_x, train_y,
+                eval_set=[(train_x, train_y), (valid_x, valid_y)],
+                callbacks=callback,
+                eval_metric='auc')
+
+        oof_preds[valid_idx] = clf.predict_proba(valid_x, num_iteration=clf.best_iteration_)[:, 1]
+        sub_preds += clf.predict_proba(test_df[feats], num_iteration=clf.best_iteration_)[:, 1] / folds.n_splits
+
+        fold_importance_df = pd.DataFrame()
+        fold_importance_df["feature"] = feats
+        fold_importance_df["importance"] = clf.feature_importances_
+        fold_importance_df["fold"] = n_fold + 1
+        feature_importance_df = pd.concat([feature_importance_df, fold_importance_df])
+        print('Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(valid_y, oof_preds[valid_idx])))
+        del clf, train_x, train_y, valid_x, valid_y
+        gc.collect()
+
+    print('Full AUC score %.6f' % roc_auc_score(train_df['TARGET'], oof_preds))
+    # Write submission file and plot feature importance
+    if not debug:
+        test_df.loc[:, 'TARGET'] = sub_preds
+        test_df[['SK_ID_CURR', 'TARGET']].to_csv(submission_file_name, index=False)
+
+    top_features = display_importances(feature_importance_df)
+    top_features = feature_correlation(train_df, top_features)
+    return feature_importance_df, top_features
+
+
+def display_importances(feature_importance_df_):
+    """
+    Function to display on a plot the feature importance and save the resulting image
+    :param feature_importance_df_:
+    :return:
+    """
+    cols = feature_importance_df_[["feature", "importance"]].groupby("feature").mean().sort_values(by="importance",
+                                                                                                   ascending=False)[
+           :40].index
+    best_features = feature_importance_df_.loc[feature_importance_df_.feature.isin(cols)]
+
+    best_feat = best_features.feature.values
+
+    plt.figure(figsize=(8, 10))
+    sns.barplot(x="importance", y="feature", data=best_features.sort_values(by="importance", ascending=False))
+    plt.title('LightGBM Features (avg over folds)')
+    plt.tight_layout()
+    plt.savefig(r'..\03_IMAGES\lgbm_importances01.png')
+    return best_feat
+
+
+def feature_correlation(train, top_feat):
+    """
+    Function to determine the correlation between features
+    :param train:
+    :param top_feat:
+    :return:
+    """
+    df = train[np.unique(top_feat)]
+    corr_matrix = df.corr().abs()
+
+    upper = corr_matrix.where(np.triu(np.ones(corr_matrix.shape), k=1).astype(bool))
+
+    # Find index of feature columns with correlation greater than 0.95
+    high_cor = [column for column in upper.columns if any(upper[column] > 0.95)]
+    features = [i for i in np.unique(top_feat) if i not in high_cor]
+    return features
+
+
+def adjust_string(string):
+    """
+    adjust string to remove special characters
+    :param string:
+    :return:
+    """
+    string = string.replace('/', '')
+    string = string.replace('  ', '_')
+    string = string.replace(' ', '_')
+    string = string.replace(',', '_')
+    string = string.replace(':', '')
+    string = string.replace('-', '')
+    return string
+
+
+def main(test_importance=False):
+    """
+    Main function for data import
+    :param test_importance:
+    :return:
+    """
+    if test_importance:
+        num_rows = 10000
+    else:
+        num_rows = None
+    df = application_train_test(num_rows)
+    with timer("Process bureau and bureau_balance"):
+        bureau = bureau_and_balance(num_rows)
+        print("Bureau df shape:", bureau.shape)
+        df = df.join(bureau, on='SK_ID_CURR')
+        del bureau
+        gc.collect()
+    with timer("Process previous_applications"):
+        prev = previous_applications(num_rows)
+        print("Previous applications df shape:", prev.shape)
+        df = df.join(prev, on='SK_ID_CURR')
+        del prev
+        gc.collect()
+    with timer("Process POS-CASH balance"):
+        pos = pos_cash(num_rows)
+        print("Pos-cash balance df shape:", pos.shape)
+        df = df.join(pos, on='SK_ID_CURR')
+        del pos
+        gc.collect()
+    with timer("Process installments payments"):
+        ins = installments_payments(num_rows)
+        print("Installments payments df shape:", ins.shape)
+        df = df.join(ins, on='SK_ID_CURR')
+        del ins
+        gc.collect()
+    with timer("Process credit card balance"):
+        cc = credit_card_balance(num_rows)
+        print("Credit card balance df shape:", cc.shape)
+        df = df.join(cc, on='SK_ID_CURR')
+        del cc
+        gc.collect()
+    if test_importance:
+        with timer("Run LightGBM with kfold"):
+            df.columns = [adjust_string(strg) for strg in df.columns]
+            feat_importance, top_feat = kfold_lightgbm(df, num_folds=10)
+
+        return feat_importance, top_feat, df
+    else:
+        return df
+
+
+if __name__ == '__main__':
+    feat_importance, top_feat, df = main(test_importance=True)
---
+++ 02_SCRIPTS/P7_other_functions.py
@@ -0,0 +1,219 @@
+"""
+The purpose of this python file is to regroup every functions from the external kernel
+"""
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.dummy import DummyClassifier
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
+from sklearn.metrics import fbeta_score
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class TrainTestGrid:
+    """
+    gridsearch the best parameters and display the results of the method chosen
+    """
+    def __init__(self, x, y, method='knn', n_esti=None):
+        self.x = x
+        self.y = y
+        self.method = method
+        self.n_esti = n_esti
+
+        # Classification parameters and train test sets
+        self.params = None
+        self.mthd = None
+        self.cls = None
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+
+        # Classification results
+        self.fbeta_score_ = None
+
+        self.set_params()
+        self.tt_split()
+        self.grid()
+
+        if self.n_esti:
+            self.bagged_fit_and_pred()
+        else:
+            self.fit_and_pred()
+
+        return
+
+    def set_params(self):
+        """
+        set parameters of the class
+        :return:
+        """
+        if self.method == 'knn':
+            self.knn_params()
+        elif self.method == 'svc':
+            self.svc_params()
+        elif self.method == 'sgd':
+            self.sgd_params()
+        elif self.method == 'rfc':
+            self.rfc_params()
+        elif self.method == 'dummy':
+            self.cls = DummyClassifier(random_state=0)
+        else:
+            print('Wrong method input, please make sure you entered either ["knn", "svc", "sgd", "rfc", "dummy"]')
+            return
+
+    def knn_params(self):
+        """
+        knn parameters and method
+        """
+        params = {
+            'n_neighbors': list(range(15, 30)),
+            'algorithm': ['auto', 'ball_tree', 'kd_tree', 'brute']
+        }
+        self.params = params
+        self.mthd = KNeighborsClassifier()
+
+    def svc_params(self):
+        """
+        svc parameters and method
+        """
+        params = {
+            'penalty': ['l1', 'l2'],
+            'multi_class': ['crammer_singer', 'ovr']
+        }
+        self.params = params
+        self.mthd = LinearSVC(random_state=0, max_iter=2000, tol=0.0001)
+
+    def sgd_params(self):
+        """
+        sgq parameters and method
+        """
+        params = {
+            'penalty': ['l1', 'l2'],
+            'tol': [0.0001, 0.001, 0.01, 0.1]
+        }
+        self.params = params
+        self.mthd = SGDClassifier(random_state=0)
+
+    def rfc_params(self):
+        """
+        rfc parameters and method
+        """
+        params = {
+            'criterion': ['gini', 'entropy']
+        }
+        self.params = params
+        self.mthd = RandomForestClassifier(random_state=0)
+
+    def tt_split(self):
+        """
+        train test split
+        """
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.x,
+            self.y,
+            test_size=0.33,
+            random_state=0)
+
+    def grid(self):
+        """
+        gridsearch on the params
+        """
+        if self.mthd is not None:  # Check if mthd exists to not use gridsearch on dummy
+            grid = GridSearchCV(self.mthd, param_grid=self.params)
+            grid.fit(self.X_train, self.y_train)
+            print('Grid best score :', grid.score(self.X_test, self.y_test))
+            print('Grid best params :', grid.best_params_)
+
+            self.cls = self.mthd.set_params(**grid.best_params_)
+
+    def fit_and_pred(self):
+        """
+        fit method, predict results and display confusion matrix
+        """
+        start_train_time = time.time()
+        self.cls.fit(self.X_train, self.y_train)
+        train_time = time.time() - start_train_time
+        print('{} train time : {}'.format(self.method.upper(), train_time))
+
+        pred = self.cls.predict(self.X_test)
+        true = self.y_test.values
+        self.fbeta_score_ = fbeta_score(true, pred, beta=0.5)
+        plt.figure(figsize=(10, 5))
+        sns.heatmap(confusion_matrix(true, pred), annot=True, cmap="Blues")
+        plt.title('{} confusion matrix \n score = {} \n fbeta score = {}'.format(
+            self.method.upper(),
+            self.cls.score(self.X_test, self.y_test),
+            self.fbeta_score_)
+        )
+        plt.show()
+
+    def bagged_fit_and_pred(self):
+        """
+        makes bags, fit method, predict results and display confusion matrix
+        """
+        print('Bagging with {} estimators'.format(self.n_esti))
+        bagged_cls = BaggingClassifier(self.cls, n_estimators=self.n_esti)
+        start_train_time = time.time()
+        bagged_cls.fit(self.X_train, self.y_train)
+        train_time = time.time() - start_train_time
+        print('Bagged {} train time : {}'.format(self.method.upper(), train_time))
+
+        start_train_time = time.time()
+        self.cls.fit(self.X_train, self.y_train)
+        train_time = time.time() - start_train_time
+        print('{} train time : {}'.format(self.method.upper(), train_time))
+
+        pred = self.cls.predict(self.X_test)
+        pred_bag = bagged_cls.predict(self.X_test)
+        true = self.y_test.values
+
+        self.fbeta_score_ = [fbeta_score(true, pred_bag, beta=0.5), fbeta_score(true, pred, beta=0.5)]
+        fig, ax = plt.subplots(ncols=2, figsize=(20, 5))
+
+        sns.heatmap(confusion_matrix(true, pred_bag), annot=True, cmap="Blues", ax=ax[0])
+        ax[0].set_title('bagged {} confusion matrix \n score = {} \n fbeta score = {}'.format(
+            self.method.upper(),
+            bagged_cls.score(self.X_test, self.y_test),
+            self.fbeta_score_[0])
+        )
+
+        sns.heatmap(confusion_matrix(true, pred), annot=True, cmap="Blues", ax=ax[1])
+        ax[1].set_title('{} confusion matrix \n score = {} \n fbeta score = {}'.format(
+            self.method.upper(),
+            self.cls.score(self.X_test, self.y_test),
+            self.fbeta_score_[1])
+        )
+        plt.show()
+
+
+def heatmap_print(true, pred, title):
+    """
+    display heatmap of classification results
+    :param true:
+    :param pred:
+    :param title:
+    """
+    plt.figure(figsize=(10, 5))
+    sns.heatmap(confusion_matrix(true, pred, normalize='all'), annot=True, cmap="Blues")
+    plt.title(title)
+    plt.show()
+
+
+def classify_with_proba(probability_pred, proba_0=0.5):
+    """
+    classify using probability
+    :param probability_pred:
+    :param proba_0:
+    :return:
+    """
+    pred_0 = probability_pred[0]
+    if pred_0 >= proba_0:
+        return 0
+    else:
+        return 1
