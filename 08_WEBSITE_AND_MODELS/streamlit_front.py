54f39622aaf1b3d233fb8394aa452d0e7c195eba
unknown <stevenaboudiwan@outlook.fr>
2022-04-30 14:41:44 +0200

    added manual search of host if automatic host not found

--- 08_WEBSITE_AND_MODELS/streamlit_front.py
+++ 08_WEBSITE_AND_MODELS/streamlit_front.py
@@ -18,43 +18,52 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-host = 'https://127.0.0.1:5000'
-#host = 'https://34.228.161.133:5000'
-#host = 'https://share.streamlit.io/aboustev/opc-project_7/main/08_WEBSITE_AND_MODELS/flask_back.py'
-response = requests.get("{}/load_data".format(host))
+host_txt = st.text_input('Host', value='127.0.0.1')
+port = st.number_input('Port', value=5000)
 
-df_grant_desc, df_all_desc, all_data = response.json()
-df_grant_desc = pd.read_json(df_grant_desc)
-df_all_desc = pd.read_json(df_all_desc)
-all_data = pd.read_json(all_data)
-all_columns = df_all_desc.columns
-
-selected = option_menu(None, ["Client loan status", "Detailed", "Semi-detailed", 'Custom simulation'],
-                       icons=['bank2', "briefcase-fill", 'briefcase', 'gear'], menu_icon="cast",
-                       orientation="horizontal")
-
-with st.sidebar:
-    client_id = int(df_all_desc.loc['min', 'SK_ID_CURR'])
-    client_id = st.number_input('Client ID',
-                                value=client_id,
-                                min_value=int(df_all_desc.loc['min', 'SK_ID_CURR']),
-                                max_value=int(df_all_desc.loc['max', 'SK_ID_CURR']),
-                                step=1)
-if client_id:
-    response = requests.get("{}/api/getdecision/?id={}".format(host, client_id))
-else:
-    val = int(all_data.loc['min', 'SK_ID_CURR'])
-    response = requests.get("{}/api/getdecision/?id={}".format(host, val))
 try:
-    client_info = pd.DataFrame.from_dict(response.json())
+    host = "127.0.0.1:5000"
+    response = requests.get("{}/load_data".format(host))
+except:
+    host = "{}:{}".format(host_txt, port)
+    try:
+        response = requests.get("{}/load_data".format(host))
+    except:
+        response = None
 
-    if selected == "Client loan status":
-        loan_status_page_front(0.75, client_info, client_id)
-    if selected == "Detailed":
-        client_id = detailed_page_front(client_info, df_grant_desc, df_all_desc, all_columns, client_id)
-    if selected == "Semi-detailed":
-        semi_detailed_page_front(all_data, client_info, client_id)
-    if selected == "Custom simulation":
-        custom_simu_page_front()
-except JSONDecodeError:
-    st.write('no data found')
+if response:
+    df_grant_desc, df_all_desc, all_data = response.json()
+    df_grant_desc = pd.read_json(df_grant_desc)
+    df_all_desc = pd.read_json(df_all_desc)
+    all_data = pd.read_json(all_data)
+    all_columns = df_all_desc.columns
+
+    selected = option_menu(None, ["Client loan status", "Detailed", "Semi-detailed", 'Custom simulation'],
+                           icons=['bank2', "briefcase-fill", 'briefcase', 'gear'], menu_icon="cast",
+                           orientation="horizontal")
+
+    with st.sidebar:
+        client_id = int(df_all_desc.loc['min', 'SK_ID_CURR'])
+        client_id = st.number_input('Client ID',
+                                    value=client_id,
+                                    min_value=int(df_all_desc.loc['min', 'SK_ID_CURR']),
+                                    max_value=int(df_all_desc.loc['max', 'SK_ID_CURR']),
+                                    step=1)
+    if client_id:
+        response = requests.get("{}/api/getdecision/?id={}".format(host, client_id))
+    else:
+        val = int(all_data.loc['min', 'SK_ID_CURR'])
+        response = requests.get("{}/api/getdecision/?id={}".format(host, val))
+    try:
+        client_info = pd.DataFrame.from_dict(response.json())
+
+        if selected == "Client loan status":
+            loan_status_page_front(0.75, client_info, client_id)
+        if selected == "Detailed":
+            client_id = detailed_page_front(client_info, df_grant_desc, df_all_desc, all_columns, client_id)
+        if selected == "Semi-detailed":
+            semi_detailed_page_front(all_data, client_info, client_id)
+        if selected == "Custom simulation":
+            custom_simu_page_front()
+    except JSONDecodeError:
+        st.write('no data found')
