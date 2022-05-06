578b40e06d3d6212ad4ac62a1e9dc27d813d4609
unknown <stevenaboudiwan@outlook.fr>
2022-05-06 10:53:00 +0200

    small adj for heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi.py
+web: gunicorn flask_app.py
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from 08_WEBSITE_AND_MODELS.flask_back import app
+import sys
+
+sys.path.insert(0, '08_WEBSITE_AND_MODELS')
+
+from flask_back import app
 
 if __name__ == "__main__":
     app.run()
